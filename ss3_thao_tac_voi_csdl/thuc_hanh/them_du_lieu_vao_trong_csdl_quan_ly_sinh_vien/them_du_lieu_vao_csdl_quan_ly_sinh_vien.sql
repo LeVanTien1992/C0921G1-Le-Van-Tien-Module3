17bb563a888df02de7d0391585ff280f4e80ede5
Your Name <vantienam@gmail.com>
2021-12-11 19:38:33 +0700

    upbai

---
+++ ss3_thao_tac_voi_csdl/thuc_hanh/them_du_lieu_vao_trong_csdl_quan_ly_sinh_vien/them_du_lieu_vao_csdl_quan_ly_sinh_vien.sql
@@ -0,0 +1,28 @@
+USE Quan_Ly_Sinh_Vien;
+
+-- INSERT INTO Class
+-- VALUES (1, 'A1', '2008-12-20', 1);
+-- INSERT INTO Class
+-- VALUES (2, 'A2', '2008-12-22', 1);
+-- INSERT INTO Class
+-- VALUES (3, 'B3', current_date, 0);
+SELECT * FROM quan_ly_sinh_vien.class;
+
+-- INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+-- VALUES ('Hung', 'Ha Noi', '0912113113', 1, 1);
+-- INSERT INTO Student (StudentName, Address, Status, ClassId)
+-- VALUES ('Hoa', 'Hai phong', 1, 1);
+-- INSERT INTO Student (StudentName, Address, Phone, Status, ClassId)
+-- VALUES ('Manh', 'HCM', '0123123123', 0, 2);
+SELECT * FROM quan_ly_sinh_vien.Student;
+-- INSERT INTO Subject
+-- VALUES (1, 'CF', 5, 1),
+--        (2, 'C', 6, 1),
+--        (3, 'HDJ', 5, 1),
+--        (4, 'RDBMS', 10, 1);
+SELECT * FROM quan_ly_sinh_vien.Subject;
+-- INSERT INTO Mark (SubId, StudentId, Mark, ExamTimes)
+-- VALUES (1, 1, 8, 1),
+--        (1, 2, 10, 2),
+--        (2, 1, 12, 1);
+SELECT * FROM quan_ly_sinh_vien.Mark;
