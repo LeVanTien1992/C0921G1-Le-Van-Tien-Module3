664b7fc5a6ce1716b4e01fe492f29f384370b766
Your Name <vantienam@gmail.com>
2021-12-13 11:01:03 +0700

    update

--- ss3_thao_tac_voi_csdl/bai_tap/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien.sql
+++ ss3_thao_tac_voi_csdl/bai_tap/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien/truy_van_du_lieu_voi_csdl_quan_ly_sinh_vien.sql
@@ -1,22 +1,23 @@
 USE Quan_Ly_Sinh_Vien;
-
+-- Hiển thị tất cả các sinh viên có tên bắt đầu bảng ký tự ‘h’
 select *
 from student
-where StudentName like "H%"; 
-
+where StudentName like "h%"; 
+-- Hiển thị các thông tin lớp học có thời gian bắt đầu vào tháng 12.
 select *
 from class
 where month(class.StartDate);
-
+-- Hiển thị tất cả các thông tin môn học có credit trong khoảng từ 3-5.
 select *
 from subject
 where Credit between 3 and 5; 
-
--- update student
--- set student.ClassId = 2
--- where student.StudentId = 1;
+-- Thay đổi mã lớp(ClassID) của sinh viên có tên ‘Hung’ là 2.
+update student
+set student.ClassId = 2
+where student.StudentId = 1;
 SELECT * FROM quan_ly_sinh_vien.student;
-
+-- Hiển thị các thông tin: StudentName, SubName, Mark. Dữ liệu sắp xếp theo điểm thi 
+-- (mark) giảm dần. nếu trùng sắp theo tên tăng dần.
 select S.StudentId, S.StudentName, Sub.SubName, M.Mark
 from Student S join Mark M on S.StudentId = M.StudentId join Subject Sub on M.SubId = Sub.SubId
-order by Mark desc;
+order by Mark desc ,  S.StudentName desc;
