3cc7d3ce8de136cfbb895f84f08c249af0fc2e75
Your Name <vantienam@gmail.com>
2021-12-14 11:04:44 +0700

    update

--- case_study_database/case_study_file.sql
+++ case_study_database/case_study_file.sql
@@ -279,17 +279,27 @@ group by dvdk.ma_dich_vu_di_kem, dvdk.ten_dich_vu_di_kem;
 
 -- 14.Hiển thị thông tin tất cả các Dịch vụ đi kèm chỉ mới được sử dụng một lần duy nhất. Thông tin hiển thị bao gồm ma_hop_dong,
 --  ten_loai_dich_vu, ten_dich_vu_di_kem, so_lan_su_dung (được tính dựa trên việc count các ma_dich_vu_di_kem).
-select hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(dvdk.ma_dich_vu_di_kem)
+select distinct hd.ma_hop_dong,  ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem, count(dvdk.ma_dich_vu_di_kem)'so lan su dung'
 from hop_dong hd  join hop_dong_chi_tiet hdct
 on hd.ma_hop_dong = hdct.ma_hop_dong  join dich_vu_di_kem dvdk
 on hdct.ma_dich_vu_di_kem = dvdk.ma_dich_vu_di_kem  join dich_vu dv
 on hd.ma_dich_vu = dv.ma_dich_vu  join loai_dich_vu ldv
 on dv.ma_loai_dich_vu = ldv.ma_loai_dich_vu
-group by hd.ma_hop_dong, ldv.ten_loai_dich_vu, dvdk.ten_dich_vu_di_kem
+group by  dvdk.ten_dich_vu_di_kem
+order by hd.ma_hop_dong asc;
 
+-- 15.	Hiển thi thông tin của tất cả nhân viên bao gồm ma_nhan_vien, ho_ten, ten_trinh_do, ten_bo_phan, so_dien_thoai, 
+-- dia_chi mới chỉ lập được tối đa 3 hợp đồng từ năm 2020 đến 2021.
+select nv.ma_nhan_vien, nv.ho_ten, td.ten_trinh_do, bp.ten_bo_phan, nv.so_dien_thoai, nv.dia_chi
+from nhan_vien nv join trinh_do td
+on nv.ma_trinh_do = td.ma_trinh_do join bo_phan bp
+on nv.ma_bo_phan = bp.ma_bo_phan join hop_dong hd 
+on nv.ma_nhan_vien = hd.ma_nhan_vien 
+where year(hd.ngay_lam_hop_dong) between 2020 and 2021 
+group by nv.ma_nhan_vien;
 
-
-
-
+-- 16.	Xóa những Nhân viên chưa từng lập được hợp đồng nào từ năm 2019 đến năm 2021.
+select *
+from 
 
 
