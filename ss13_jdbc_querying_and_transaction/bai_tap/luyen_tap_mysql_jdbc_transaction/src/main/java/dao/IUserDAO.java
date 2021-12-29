2b9119cb27566f1d43bfb33b895226b69bb326a9
Your Name <vantienam@gmail.com>
2021-12-29 10:44:27 +0700

    update

---
+++ ss13_jdbc_querying_and_transaction/bai_tap/luyen_tap_mysql_jdbc_transaction/src/main/java/dao/IUserDAO.java
@@ -0,0 +1,19 @@
+package dao;
+
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IUserDAO {
+    public void insertUser(com.codegym.model.User user) throws SQLException;
+
+    public com.codegym.model.User selectUser(int id);
+
+    public List<com.codegym.model.User> selectAllUsers();
+
+    public boolean deleteUser(int id) throws SQLException;
+
+    public boolean updateUser(com.codegym.model.User user) throws SQLException;
+    com.codegym.model.User getUserById(int id);
+
+    void insertUserStore(com.codegym.model.User user) throws SQLException;
+}
